a8164073a9595ab9d1ab7c9a845a759e5e45efe0
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-03-18 21:08:46 +0200

    Update styling details in ProfileCardVanilla and ProfileCardStyled

--- src/components/ProfileCardStyled.jsx
+++ src/components/ProfileCardStyled.jsx
@@ -1,36 +1,39 @@
 import styled from 'styled-components';
 
 const Card = styled.div`
-    background-color: rgb(225, 225, 225);
-    border: 1px solid #ccc;
-    border-radius: 10px;
+    background-color:  #f5f5f5;
+    border: 1px solid #e0e0e0;
+    border-radius: 8px;
     padding: 20px;
     text-align: center;
 `
 
 const Avatar = styled.img`
-    border-radius: 50%;
+    border-radius: 50px;
     margin-bottom: 10px;
 `
 
 const Name = styled.h2`
-    font-size: 1.5rem;
+    font-size: 20px;
     color: black;
+    font-weight: bold;
     margin-bottom: 8px;
 `
 
 const Text = styled.p`
     font-family: Arial, Helvetica, sans-serif;
-    font-size: 0.8rem;
-    color: #737373;
+    font-size: 14px;
+    color: #666;
 `
 
 const Button = styled.button`
-    background-color: #326eea;
+    background-color: #1976d2;
     color: white;
+    width: 120px;
+    border-right: 4px;
     padding: 8px 15px;
+    margin-bottom: 20px;
     border: none;
-    border-radius: 5px;
 `
 
 const ProfileCardStyled = () => {
