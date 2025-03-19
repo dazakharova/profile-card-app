1fe7ac0643e633d57416e7fe16db11416ba21d34
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-03-19 10:55:48 +0200

    Set width (300px) for ProfileCard components

--- src/components/ProfileCardStyled.jsx
+++ src/components/ProfileCardStyled.jsx
@@ -4,6 +4,7 @@ const Card = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    width: 300px;
     background-color:  #f5f5f5;
     border: 1px solid #e0e0e0;
     border-radius: 8px;
--- src/components/ProfileCardTailwind.jsx
+++ src/components/ProfileCardTailwind.jsx
@@ -2,7 +2,7 @@ import '../styles/ProfileCardTailwind.css';
 
 const ProfileCardTailwind = () => {
   return (
-      <div className="flex flex-col items-center bg-[#f5f5f5] border border-[#e0e0e0] p-5 rounded-[8px] text-center mx-auto">
+      <div className="flex flex-col items-center w-[300px] bg-[#f5f5f5] border border-[#e0e0e0] p-5 rounded-[8px] text-center mx-auto">
         <img src="https://picsum.photos/100/100" alt="avatar" className="w-[100px] h-[100px] rounded-full mb-[10px]" />
         <h2 className="text-[20px] font-bold text-black mb-[8px]">Matti Meikäläinen</h2>
         <p className="font-[Arial] text-[14px] text-[#666]">Full-stack developer, passionate about coding and coffee</p>
